c6dbb8efc3bac07b0a67e4d5907bb5d70d469ff1
ianhungvc <shaoyenhung@gmail.com>
2015-08-25 23:31:16 -0700

    Create citibike.py

---
+++ citibike.py
@@ -0,0 +1,22 @@
+import time
+from dateutil.parser import parse
+import collections
+import sqlite3 as lite
+import requests
+
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+
+r = requests.get('http://www.citibikenyc.com/stations/json')
+exec_time = parse(r.json()['executionTime'])
+
+cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%s'),))
+con.commit()
+
+id_bikes = collections.defaultdict(int)
+for station in r.json()['stationBeanList']:
+    id_bikes[station['id']] = station['availableBikes']
+
+for k, v in id_bikes.iteritems():
+    cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = " + exec_time.strftime('%s') + ";")
+con.commit()
